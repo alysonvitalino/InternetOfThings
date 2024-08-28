0cf4034df8224f729618e85c43b0308df7420ee8
alysonvitalino <162512614+alysonvitalino@users.noreply.github.com>
2024-08-28 20:39:44 -0300

    Update Semaforo_botao_Alyson.ino

--- Semaforo_botao_Alyson.ino
+++ Semaforo_botao_Alyson.ino
@@ -1,22 +1,35 @@
 // C++ code
 //
-int botao = 0;
+int botaosemaforo = 0;
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, INPUT); // BOTÃO
+  pinMode(7, OUTPUT); // VERDE
+  pinMode(4, OUTPUT); // AMARELO
+  pinMode(1, OUTPUT); // VERMELHO
+  pinMode(11, OUTPUT); // PEDESTRE VERDE
+  pinMode(9, OUTPUT); // PEDESTRE VERMELHO
 }
 
 void loop()
 {
-  botao = digitalRead(10);
-  if (botao == HIGH)
+  
+  botaosemaforo = digitalRead(13);
+  if (botaosemaforo == HIGH)
   {
+    digitalWrite(7, HIGH);
+    digitalWrite(9, HIGH);
+    delay(3000);
+    digitalWrite(7, LOW);
+    digitalWrite(4, HIGH);
+    delay(1000);
+    digitalWrite(4, LOW);
+    digitalWrite(9, LOW);
     digitalWrite(1, HIGH);
-    delay(1000);
-  } 
-  else
-  {
-    digitalWrite(1, LOW);
-    delay(1000);
+    delay(500);
+    digitalWrite(11, HIGH);
+    delay(1500);
+    digitalWrite(11,LOW);
+    digitalWrite(1,LOW);
   }
 }
